4fc6f0d9e803ffe01baf6ddc8ef6280d55a6665d
Abanoub Barayo <62501003+abanoubdev@users.noreply.github.com>
2026-04-23 15:32:12 +0200

    clean out hidden gems

--- SQL/hidden_gems.sql
+++ SQL/hidden_gems.sql
@@ -9,7 +9,6 @@ WITH avg_drg_billing AS (
 )
 SELECT 
     h.hospital_name,
-	r.drg_code,
 	r.drg_description,
 	h.rating AS star_rating,
 	b.avg_total_payment AS hospital_price,
@@ -17,12 +16,14 @@ SELECT
     round(avg_drg_billing.avg_payment - b.avg_total_payment, 2) AS total_savings,
 	l.city, l.state,
     l.state_full
+    
 FROM hospitals h
 LEFT JOIN billing b ON h.facility_id = b.facility_id
 LEFT JOIN locations l ON h.location_id = l.location_id
 LEFT JOIN ref_drg r ON b.drg_code = r.drg_code
 LEFT JOIN avg_drg_billing ON b.drg_code = avg_drg_billing.drg_code
 WHERE 
-	h.rating = 5 AND b.avg_total_payment < avg_drg_billing.avg_payment AND l.state='CA'
-	
-ORDER BY l.state_full,total_savings DESC
+	h.rating = 5
+  AND 
+	b.avg_total_payment < avg_drg_billing.avg_payment
+ORDER BY r.drg_code,total_savings DESC;
