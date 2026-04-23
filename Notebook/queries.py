cfb9fb6ed7fffad91dac1d5bada92f140431207f
Abanoub Barayo <62501003+abanoubdev@users.noreply.github.com>
2026-04-23 13:08:48 +0200

    clean up queries and push it to queries file

---
+++ Notebook/queries.py
@@ -0,0 +1,33 @@
+def get_hospital_query(location_input):
+    query = f"""
+    WITH avg_drg_billing AS (
+        SELECT 
+            drg_code,
+            AVG(avg_total_payment) AS avg_payment
+        FROM billing
+        GROUP BY drg_code
+    )
+    SELECT 
+        h.hospital_name,
+        r.drg_code,
+        r.drg_description,
+        h.rating AS star_rating,
+        b.avg_total_payment AS hospital_price,
+        round(avg_drg_billing.avg_payment, 2) AS national_avg_price,
+        round(avg_drg_billing.avg_payment - b.avg_total_payment, 2) AS total_savings,
+        l.city, 
+        l.state,
+        l.state_full
+    FROM hospitals h
+    LEFT JOIN billing b ON h.facility_id = b.facility_id
+    LEFT JOIN locations l ON h.location_id = l.location_id
+    LEFT JOIN ref_drg r ON b.drg_code = r.drg_code
+    LEFT JOIN avg_drg_billing ON b.drg_code = avg_drg_billing.drg_code
+    WHERE 
+        h.rating = 5 
+        AND b.avg_total_payment < avg_drg_billing.avg_payment 
+        AND (l.state = '{location_input}' OR l.city = '{location_input}' OR l.state_full = '{location_input}')
+    ORDER BY total_savings DESC
+    LIMIT 20;
+    """
+    return query
