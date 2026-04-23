b368230b176d64268b7dc10407269473e7aba780
Abanoub Barayo <62501003+abanoubdev@users.noreply.github.com>
2026-04-23 11:50:25 +0200

    Modify hidden gems sql using cte

--- SQL/hidden_gems.sql
+++ SQL/hidden_gems.sql
@@ -1,24 +1,28 @@
 USE hospital_db;
 
-
-SELECT 
-    h.hospital_name,
-    l.city,
-    l.state,
-    h.rating AS star_rating,
-    r.drg_description,
-    b.avg_total_payment AS hospital_price,
-    round(avg_table.national_avg, 2) AS national_avg_price,
-    round(avg_table.national_avg - b.avg_total_payment, 2) AS total_savings
-FROM hospitals h
-JOIN billing b ON h.facility_id = b.facility_id
-JOIN locations l ON h.location_id = l.location_id
-JOIN ref_drg r ON b.drg_code = r.drg_code
-JOIN (
-    SELECT drg_code, AVG(avg_total_payment) AS national_avg
+WITH avg_drg_billing AS (
+	SELECT 
+    drg_code,
+    AVG(avg_total_payment) AS avg_payment
     FROM billing
     GROUP BY drg_code
-) AS avg_table ON b.drg_code = avg_table.drg_code
-WHERE h.rating = 5 
-  AND b.avg_total_payment < avg_table.national_avg
-ORDER BY total_savings DESC;
+)
+SELECT 
+    h.hospital_name,
+	r.drg_description,
+	h.rating AS star_rating,
+	b.avg_total_payment AS hospital_price,
+	round(avg_drg_billing.avg_payment, 2) AS national_avg_price,
+    round(avg_drg_billing.avg_payment - b.avg_total_payment, 2) AS total_savings,
+	l.city, l.state,
+    l.state_full
+FROM hospitals h
+LEFT JOIN billing b ON h.facility_id = b.facility_id
+LEFT JOIN locations l ON h.location_id = l.location_id
+LEFT JOIN ref_drg r ON b.drg_code = r.drg_code
+LEFT JOIN avg_drg_billing ON b.drg_code = avg_drg_billing.drg_code
+WHERE 
+	h.rating = 5 
+  AND 
+	b.avg_total_payment < avg_drg_billing.avg_payment
+ORDER BY l.state_full,total_savings DESC
